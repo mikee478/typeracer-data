d1fdd3d0ef931a799a039e724a5d8dc797d8dcae
Mikee <michaelwolfsonkin@gmail.com>
2022-03-01 22:42:07 -0500

    Requests update and waits until page data has updated

--- typeracer_data.py
+++ typeracer_data.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import datetime as dt
 from bs4 import BeautifulSoup as bs
 import requests
+import time
+import datetime as dt
+from dateutil import parser
+import pytz
 
 def send_update_request(username):
 	url = 'https://typeracerdata.com/import?username=' + username
@@ -28,28 +31,55 @@ def parse_page_text(text):
 
 	return np.array(data, dtype=[('Date', 'U32'), ('Average WPM', float), ('Best WPM', float), ('Races', int), ('Wins', int), ('Win %', float)])	
 
-if __name__ == '__main__':
-	
-	username = 'mikee478'
-	if send_update_request(username):
+def get_updated_text(username, max_sec_since_update=30, max_wait_time=10, sleep_time=1):
+	if not send_update_request(username): return None
+
+	n_seconds = dt.timedelta(seconds=max_sec_since_update)
+
+	start_time = time.time()
+
+	while time.time() - start_time <= max_wait_time:
 		page_text = get_page_text(username)
-		if page_text:
-			data = parse_page_text(page_text)
+		if page_text is not None:
+			last_imp_ind1 = page_text.find('(Last import: ')
+			last_imp_ind2 = page_text.find(')', last_imp_ind1)
 
-			fig, ax = plt.subplots()
+			date_str = page_text[last_imp_ind1+14:last_imp_ind2]
+			last_update = parser.parse(date_str)
 
-			x = [dt.datetime.strptime(d,'%B %Y').date() for d in data['Date']]
-			y = data['Average WPM']
-			ax.plot(x,y,'-ob', label='Average WPM')
-			y = data['Best WPM']
-			ax.plot(x,y,'-or', label='Best WPM')
+			now = dt.datetime.now(pytz.utc)
 
-			fig.canvas.manager.set_window_title('TypeRacer Data')
-			ax.set_title(f'User Data For {username}')
-			ax.legend()
-			ax.xaxis.set_major_formatter(mdates.DateFormatter('%B %Y'))
-			ax.xaxis.set_major_locator(mdates.MonthLocator(interval=4))
-			ax.set_ylabel('WPM')
-			fig.autofmt_xdate()
+			if now - last_update < n_seconds:
+				return page_text, last_update
 
-			plt.show()
+		time.sleep(sleep_time)
+
+	return None
+
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=dt.timezone.utc).astimezone(tz=None)
+
+def plot_data(data, last_update):
+	fig, ax = plt.subplots()
+
+	x = [dt.datetime.strptime(d,'%B %Y').date() for d in data['Date']]
+	y = data['Average WPM']
+	ax.plot(x,y,'-ob', label='Average WPM')
+	y = data['Best WPM']
+	ax.plot(x,y,'-or', label='Best WPM')
+
+	fig.canvas.manager.set_window_title('TypeRacer Data')
+	ax.set_title(f'User Data For {username} (Last Updated {utc_to_local(last_update).strftime("%x %-I:%M:%S %p")})')
+	ax.legend()
+	ax.xaxis.set_major_formatter(mdates.DateFormatter('%B %Y'))
+	ax.xaxis.set_major_locator(mdates.MonthLocator(interval=4))
+	ax.set_ylabel('WPM')
+	fig.autofmt_xdate()
+
+	plt.show()
+
+if __name__ == '__main__':
+	username = 'mikee478'
+	page_text, last_update = get_updated_text(username)
+	data = parse_page_text(page_text)
+	plot_data(data, last_update)
